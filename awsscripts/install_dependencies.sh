4fa1815fa692601bd10bbad8945ccda817ce4d6f
vigyat123 <32060077+vigyat123@users.noreply.github.com>
2017-10-30 11:45:31 +0530

    Update install_dependencies.sh

--- awsscripts/install_dependencies.sh
+++ awsscripts/install_dependencies.sh
@@ -8,3 +8,12 @@ if [[ (-d $CATALINA_HOME) && (service tomcat7 status) ]]; then
 else
 sudo yum install tomcat7 tomcat7-webapps
 fi
+NODE_HOME=/home/ec2-user/.nvm/versions/node
+if [ -d $NODE_HOME ]; then
+
+else
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+ nvm install 6.11.5   
+ node -e "console.log('Running Node.js ' + process.version)"
+fi

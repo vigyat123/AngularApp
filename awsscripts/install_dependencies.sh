cb45f0ec3c91ee5a5ceee032459e6f262e76a433
vigyat123 <32060077+vigyat123@users.noreply.github.com>
2017-10-30 17:23:02 +0530

    Update install_dependencies.sh

--- awsscripts/install_dependencies.sh
+++ awsscripts/install_dependencies.sh
@@ -2,12 +2,7 @@
 
 set -e
 
-CATALINA_HOME=/usr/share/tomcat7
-if [[ (-d $CATALINA_HOME) && (service tomcat7 status) ]]; then
-    echo "Tomcat7 is already installed at $CATALINA_HOME. Skip reinstalling it."
-else
 sudo yum install tomcat7 tomcat7-webapps
-fi
 NODE_HOME=/home/ec2-user/.nvm/versions/node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 . ~/.nvm/nvm.sh

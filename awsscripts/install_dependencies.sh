78e4b64431d2247493c95643d565054a4a0a3122
vigyat123 <32060077+vigyat123@users.noreply.github.com>
2017-10-30 10:43:34 +0530

    Update install_dependencies.sh

--- awsscripts/install_dependencies.sh
+++ awsscripts/install_dependencies.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+set -e
+
+CATALINA_HOME=/usr/share/tomcat7
+if [[ (-d $CATALINA_HOME) && (service tomcat7 status) ]]; then
+    echo "Tomcat7 is already installed at $CATALINA_HOME. Skip reinstalling it."
+else
+sudo yum install tomcat7 tomcat7-webapps
+fi

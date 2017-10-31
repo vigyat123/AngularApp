2b12528cffcc7c55386f2134956ef8401f9673b3
vigyat123 <32060077+vigyat123@users.noreply.github.com>
2017-10-31 17:48:06 +0530

    Update start_application.sh

--- awsscripts/start_application.sh
+++ awsscripts/start_application.sh
@@ -3,7 +3,7 @@
 set -e
 sudo su
 TEMP_STAGING_DIR='/tmp/codedeploy-deployment-staging-area'
-WAR_STAGED_LOCATION="$TEMP_STAGING_DIR/dist"
+WAR_STAGED_LOCATION="$TEMP_STAGING_DIR/dist/*"
 CONTEXT_PATH='ROOT'
 if [[ -d $CATALINA_HOME/webapps/$CONTEXT_PATH ]]; then
     rm -rfv $CATALINA_HOME/webapps/$CONTEXT_PATH
